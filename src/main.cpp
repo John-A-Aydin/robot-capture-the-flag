1e24215da769ef20863af9d8e94cb3b5c78de83d
John-A-Aydin <johnaaydin@gmail.com>
2024-05-10 09:29:45 -0400

    Added functions for drawing dashed and dotted circles

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,8 @@ void    input();
 void    write_score(SDL_Renderer* renderer, int trials, int robot1_wins, int robot2_wins, SDL_Color color, TTF_Font* font);
 int     SDL_RenderFillCircle(SDL_Renderer * renderer, int x, int y, int radius);
 int     SDL_RenderDrawCircle(SDL_Renderer * renderer, int x, int y, int radius);
+int     SDL_RenderDrawDottedCircle(SDL_Renderer * renderer, int x, int y, int radius, int spacing);
+int     SDL_RenderDrawDashedCircle(SDL_Renderer * renderer, int x, int y, int radius, int spacing);
 void    render_robots(SDL_Renderer * renderer, pair<double, double> r1, pair<double, double> r2, SDL_Color r1_color, SDL_Color r2_color);
 void    render_game_area(SDL_Renderer * renderer, SDL_Color color);
 void    render_target(SDL_Renderer * renderer, pair<double, double> target, SDL_Color color);
@@ -114,7 +116,7 @@ int main() {
         frames_in_state++;
         render_game_area(renderer, main_color);
         render_robots(renderer, robot1, robot2, r1_color, r2_color);
-        
+        //SDL_RenderDrawDashedCircle(renderer, WIDTH/2 + PADDING, HEIGHT/2 , WIDTH/4, 8);
         write_score(renderer, trials, robot1_wins, robot2_wins, main_color, font);
         switch (game_state)
         {
@@ -443,6 +445,107 @@ int SDL_RenderFillCircle(SDL_Renderer * renderer, int x, int y, int radius) {
                                      x + offsetx, y - offsety);
         status += SDL_RenderDrawLine(renderer, x - offsety, y - offsetx,
                                      x + offsety, y - offsetx);
+
+        if (status < 0) {
+            status = -1;
+            break;
+        }
+
+        if (d >= 2*offsetx) {
+            d -= 2*offsetx + 1;
+            offsetx +=1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
+
+    return status;
+}
+
+int SDL_RenderDrawDottedCircle(SDL_Renderer * renderer, int x, int y, int radius, int spacing) {
+    int offsetx, offsety, d;
+    int status;
+    int count;
+
+    offsetx = 0;
+    offsety = radius;
+    d = radius -1;
+    status = 0;
+    count = 0;
+
+    while (offsety >= offsetx) {
+        if (count % spacing == 0) {
+            status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
+            status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
+            status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
+            status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
+            status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
+            status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
+            status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
+            status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
+        }
+        count++;
+        if (status < 0) {
+            status = -1;
+            break;
+        }
+
+        if (d >= 2*offsetx) {
+            d -= 2*offsetx + 1;
+            offsetx +=1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
+
+    return status;
+}
+
+int SDL_RenderDrawDashedCircle(SDL_Renderer * renderer, int x, int y, int radius, int spacing) {
+    int offsetx, offsety, d;
+    int status;
+    int count;
+    bool draw;
+
+    offsetx = 0;
+    offsety = radius;
+    d = radius -1;
+    status = 0;
+    count = 0;
+    draw = true;
+    
+
+    while (offsety >= offsetx) {
+        if (draw) {
+            status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
+            status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
+            status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
+            status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
+            status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
+            status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
+            status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
+            status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
+        }
+        count++;
+        if (count >= spacing) {
+            draw = !draw;
+            count = 0;
+        }
+            
 
         if (status < 0) {
             status = -1;
